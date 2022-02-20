820a56c949bcab6b8921e4e16b10a0647dddb90d
UndeadStar <BoomerangAide@users.noreply.github.com>
2022-02-20 22:59:52 +0100

    Update password_manager_button.uc.js
    Made it use an icon from CustomCSSforFx.
    Changed to french texts.
    Removed things for older versions because for personal use my version was guaranteed.
    I think I may have tried to get the middle click to work and failed miserably, if changes related to that are still in that file.

--- scripts/password_manager_button.uc.js
+++ scripts/password_manager_button.uc.js
@@ -6,23 +6,25 @@
 // default: left-click on password manager button opens new password manager in a tab and adds current (non-chrome) domain/host into input field
 // default: middle-click on password manager button opens 'chrome://passwordmgr/content/passwordManager.xul/xhtml' in a popup
 
-(function() {
+var CustomCSSforFx_icon_filename = 'padlock_classic.png'; //change to the filename (without path) of an icon from CustomCSSforFx
 
-try {
+(function() {
+		 
+		try {
   Components.utils.import("resource:///modules/CustomizableUI.jsm");
   Components.utils.import("resource://gre/modules/LoginHelper.jsm");
   var {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
-  var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
-  
-  var leftclick_opens_old_pw_manager = false;
-  var middleclick_opens_old_pw_manager = true;
-  
-  CustomizableUI.createWidget({
-	id: "pw_manager_button", // button id
-	defaultArea: CustomizableUI.AREA_NAVBAR,
-	removable: true,
-	label: "Open Password Manager", // button title
-	tooltiptext: "Open Password Manager", // tooltip title
+	  var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+	  
+	  var leftclick_opens_old_pw_manager = false;
+	  var middleclick_opens_old_pw_manager = true;
+	  
+	  CustomizableUI.createWidget({
+		id: "pw_manager_button", // button id
+		defaultArea: CustomizableUI.AREA_NAVBAR,
+		removable: true,
+		label: "Mots de passe", // button title
+		tooltiptext: "Mots de passe", // tooltip title
 	onClick: function(event) {
   
 	  // open about:logins in a new tab and add current domain to it (excl. about pages)
@@ -38,57 +40,68 @@ try {
 	  function open_old_pw_manager() {
 		try { 
 		  var old_pw_manager = 'chrome://passwordmgr/content/passwordManager.xhtml';
-		  if(parseInt(Services.appinfo.version) < 73) old_pw_manager = 'chrome://passwordmgr/content/passwordManager.xul';
-		
 		  window.open(old_pw_manager ,'', 'chrome, resizable', "width=400,height=400");
 		} catch (e) {}
 	  }
   
-	  
-	  // left-click
-	  if(event.button=='0') {
-		
-		if(leftclick_opens_old_pw_manager) open_old_pw_manager();
-		else open_aboutlogins_in_tab();
+ 
+		  // left-click
+	  if(event.button == '0') {
+			
+		if(leftclick_opens_old_pw_manager) 
+			open_old_pw_manager();
+		else 
+				open_aboutlogins_in_tab();
 
-	  } 
-	  // right-click
-	  else if(event.button=='1') {
-		
-		if(middleclick_opens_old_pw_manager) open_old_pw_manager();
-		else open_aboutlogins_in_tab();
+		  } 
+		  // right-click
+	  else if(event.button == '1') {
+			
+		if(middleclick_opens_old_pw_manager)
+			open_old_pw_manager();
+		else
+				open_aboutlogins_in_tab();
 
-	  }
-	  
-	},
-	
+		  }
+		  
+		},
+		
 	onCreated: function(button) {
 	  return button;
-	}
-		
-  });
-  
-  /* set icon */
-  var button_icon = 'chrome://browser/skin/login.svg';
-  if (parseInt(Services.appinfo.version) < 68) button_icon = 'chrome://browser/skin/connection-secure.svg';
-  
-  // style button icon
-  var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent('\
-	\
-	  #pw_manager_button .toolbarbutton-icon {\
-		list-style-image: url("'+button_icon+'") !important; /* icon / path to icon */ \
-		fill: red !important; /* icon color name/code */\
-	  }\
-	  #customization-content-container #pw_manager_button .toolbarbutton-icon, \
-	  panelmultiview #pw_manager_button .toolbarbutton-icon {\
-		width: 18px !important; \
-		height: 18px !important; \
-	  }\
-	\
-  '), null, null);
-  
-  sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
-  
+		}
+			
+	  });
+		  
+	  /* set icon */
+	  var button_icon;
+	  
+	  if(CustomCSSforFx_icon_filename === null) {
+		  button_icon = 'chrome://browser/skin/login.svg';
+	  }
+	  else {
+		  button_icon = 'file:///' +
+				  Components.classes["@mozilla.org/file/directory_service;1"].getService( Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile).path.replace(/\134/g,'/') +
+				  '/chrome/image/' +
+				  CustomCSSforFx_icon_filename;
+	  }
+	  
+	  // style button icon
+	  var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent('\
+		\
+		  #pw_manager_button .toolbarbutton-icon {\
+			list-style-image: url("'+button_icon+'") !important; /* icon / path to icon */ \
+			fill: red !important; /* icon color name/code, red in original code */\
+		  }\
+		  #customization-content-container #pw_manager_button .toolbarbutton-icon, \
+		  panelmultiview #pw_manager_button .toolbarbutton-icon {\
+			width: 18px !important; \
+			height: 18px !important; \
+		  }\
+		\
+	  '), null, null);
+	  
+	  sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+	  
 } catch (e) {
 	Components.utils.reportError(e);
 };
