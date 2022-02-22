d1ef370f6d25fad5f84f2d5c7bea33ab095b8e57
UndeadStar <BoomerangAide@users.noreply.github.com>
2022-02-22 22:41:28 +0100

    Update userChrome.js
    Warning: my own version.
    Informative, but normally not directly useable as stated in the readme.

--- method 2/profile/userChrome.js
+++ method 2/profile/userChrome.js
@@ -1,3 +1,22 @@
 // userChrome.js
-userChrome.import("/userChrome/my_script_name.uc.js", "UChrm");
-userChrome.import("/userChrome/my_other_script_name.uc.js", "UChrm");
+
+function load_js_script(js_file_name) {
+	userChrome.import("/js_stuff/" + js_file_name, "UChrm");
+}
+
+try {
+	load_js_script("others_customizations.js");
+	load_js_script("download_button_open_list_directly.js");
+	load_js_script("favicon_in_urlbar.uc.js");
+	load_js_script("restart_item_in_menu.uc.js");
+	load_js_script("space_and_separator_restorer.uc.js");
+	load_js_script("alternative_searchbar.uc.js");
+	load_js_script("password_manager_button.uc.js");
+}
+catch(exc) {
+	console.log("Exception in userChrome.js");
+	console.log(exc);
+}
+
+// load other scripts
+// load_js_script("my_other_script_name.uc.js");
