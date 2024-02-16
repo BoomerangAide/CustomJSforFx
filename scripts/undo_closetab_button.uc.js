a6c1ef583d357caec32d285ed1a11775cd2b65d2
UndeadStar <BoomerangAide@users.noreply.github.com>
2024-02-17 00:06:24 +0100

    Update according to main project

--- scripts/undo_closetab_button.uc.js
+++ scripts/undo_closetab_button.uc.js
@@ -5,7 +5,7 @@
 
 try {
   Components.utils.import("resource:///modules/CustomizableUI.jsm");
-  var {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
+  ChromeUtils.importESModule("resource:///modules/CustomizableUI.sys.mjs");
   var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
   var appversion = parseInt(Services.appinfo.version);
   
